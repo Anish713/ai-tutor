199a5016a5cb65811c2fd80d6d7312355e44203c
Anish713 <anishstha780@gmail.com>
2024-09-08 11:36:52 +0545

    .

--- MathSpecialized/maths/dynamic_content.py
+++ MathSpecialized/maths/dynamic_content.py
@@ -1,11 +1,12 @@
-# dynamic_content.py
 # import streamlit as st
 from utils.mathutils import get_response_from_api
 from dotenv import load_dotenv
 
+
 def generate_dynamic_content_ollama(query):
     response = get_response_from_api(query)
     return response
+
 
 load_dotenv()
 
@@ -14,8 +15,6 @@ load_dotenv()
 # explanation = generate_dynamic_content(query)
 # st.write(explanation)
 
-
-# dynamic_content.py
 
 from groq import Groq
 
